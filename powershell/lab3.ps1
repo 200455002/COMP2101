90da3fdd160136ded0f09cbd759583a51539fc7e
200455002 <97916310+200455002@users.noreply.github.com>
2022-03-23 13:16:45 -0400

    lab3

---
+++ powershell/lab3.ps1
@@ -0,0 +1,2 @@
+﻿get-ciminstance -ClassName Win32_NetworkAdapterConfiguration |? {$_.ipenabled -eq "True" } |
+format-table -AutoSize Description, Index, IPAddress, IPSubnet, DNSDomain, DNSServerSearchOrder
