057308b390498e0cc81f9e7606003aeb1bb756ab
200455002 <97916310+200455002@users.noreply.github.com>
2022-04-11 13:42:53 -0400

    lab5

---
+++ powershell/LAB5.ps1
@@ -0,0 +1,21 @@
+﻿Param ( [switch]$System, 
+        [switch]$Disks,
+        [switch]$Network
+)
+
+import-module SagarRana
+
+if ($System -eq $false -and $Disks -eq $false -and $Network -eq $false) {
+ Write-Host "Insert command line arguments or it will display everything."
+ Write-Host "............................................................"
+ SagarRana-System;
+ SagarRana-Disks;
+ SagarRana-Network;
+} else {
+ if ($System) {
+     SagarRana-System }
+ if ($Disks) {
+     SagarRana-Disks; }
+ if ($Network) {
+     SagarRana-Network }
+}
